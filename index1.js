369fcc9933a86ab352b92fbb4577c93aab1eb19d
sane5ek <sane55ek@gmail.com>
2021-04-21 20:44:53 +0300

    no message

--- index1.js
+++ index1.js
@@ -1,5 +1,5 @@
 // Our input frames will come from here.
-// const videoElement = document.getElementsByClassName('input_video')[0];
+const videoElement = document.getElementsByClassName('input_video')[0];
 const canvasElement = document.getElementsByClassName('output_canvas')[0];
 const controlsElement = document.getElementsByClassName('control-panel')[0];
 // const canvasCtx = canvasElement.getContext('2d');
@@ -65,7 +65,7 @@ faceMesh.onResults(onResults);
 // Instantiate a camera. We'll feed each frame we receive into the solution.
 const camera = new Camera(videoElement, {
   onFrame: async () => {
-    await faceMesh.send({image: videoElement});
+    // await faceMesh.send({image: videoElement});
   },
   width: 1280,
   height: 720
