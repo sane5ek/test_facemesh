d93c54c40564e3a12b70f5a43d53f4d37ff898e4
sane5ek <sane55ek@gmail.com>
2021-04-21 16:59:37 +0300

    no message

--- index1.js
+++ index1.js
@@ -173,8 +173,8 @@ function passThrough(src) {
 }
 
 function deformate(src) {
-  console.log('coords', results.multiFaceLandmarks[0][0])
-  console.log('length', results.multiFaceLandmarks[0].length)
+  console.log('coords', CROSS_RESULTS.multiFaceLandmarks[0][0])
+  console.log('length', CROSS_RESULTS.multiFaceLandmarks[0].length)
   right_eye = [215,105]
   radius = 30
   power = 2
