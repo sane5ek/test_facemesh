79f755a76925df2f527e3d807b812139782689fe
sane5ek <sane55ek@gmail.com>
2021-04-21 21:43:52 +0300

    no message

--- index1.js
+++ index1.js
@@ -63,8 +63,8 @@ const camera = new Camera(videoElement, {
   onFrame: async () => {
     await faceMesh.send({image: videoElement});
   },
-  width: 1280,
-  height: 720
+  width: 640,
+  height: 480
 });
 camera.start();
 
